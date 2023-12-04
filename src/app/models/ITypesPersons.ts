0967bce2f549afef77b4ad340d88719d6d9f294f
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-04 10:14:30 -0400

    Models [Interface] - Refactor [04/12/2023]

--- src/app/models/ITypesPersons.ts
+++ src/app/models/ITypesPersons.ts
@@ -1,13 +1,13 @@
 import { Time } from "@angular/common";
 
 export interface ITypesPersons {
-    Rfrnc?:number;
-    Nm:string;
-    Dscrptn:string;
+             Rfrnc?:number;
+                 Nm:string;
+            Dscrptn:string;
     Rfrnc_TypsPrsns:number;
-    Cndtn:number;
-    Rmvd:number;
-    Lckd:number;
-    DtAdmssn:Date;
-    ChckTm:Time;
+              Cndtn:number;
+               Rmvd:number;
+               Lckd:number;
+           DtAdmssn:Date;
+             ChckTm:Time;
 }
