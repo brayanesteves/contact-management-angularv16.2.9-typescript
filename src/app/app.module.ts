6ba08dcb0f1170cb3affe29e0508b4fef4c402aa
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-07 13:16:08 -0400

    Import "FormsModule" - app.module.ts [07/12/2023]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }      from '@angular/forms';
 
 import { AppRoutingModule }        from './app-routing.module';
 import { AppComponent }            from './app.component';
@@ -26,7 +27,8 @@ import { PageNotFoundComponent }   from './components/page-not-found/page-not-fo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
