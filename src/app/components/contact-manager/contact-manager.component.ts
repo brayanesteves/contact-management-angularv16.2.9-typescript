8897cff80e83a6acbc4ae1a97e7389d0c1fa4762
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-07 11:57:58 -0400

    Component & Function - Contact Manager [07/12/2023]

--- src/app/components/contact-manager/contact-manager.component.ts
+++ src/app/components/contact-manager/contact-manager.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { INaturalPerson } from 'src/app/models/INaturalPerson';
+import { NaturalPersonService } from 'src/app/services/natural-person.service';
 
 @Component({
   selector: 'app-contact-manager',
   templateUrl: './contact-manager.component.html',
   styleUrls: ['./contact-manager.component.css']
 })
-export class ContactManagerComponent {
+export class ContactManagerComponent implements OnInit {
 
+  public        loading:boolean          = false;
+  public naturalPersons:INaturalPerson[] = [];
+  public   errorMessage:string | null    = null;
+
+  constructor(private naturalPersonService:NaturalPersonService) { }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.naturalPersonService.getNaturalPersonsAll().subscribe((data) => {
+      console.log(data)
+      this.naturalPersons = data;
+      this.loading        = false;
+    }, (error) => {
+      this.errorMessage = error;
+      this.loading      = false;
+    });
+  }
 }
