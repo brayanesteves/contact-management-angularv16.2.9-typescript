92eb3fad9fa10184229e5c21168dfdc72951a6ca
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-06 20:38:18 -0400

    Import - app.module -> HttpClientModule (@angular/common/http) [06/12/2023]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule }        from './app-routing.module';
 import { AppComponent }            from './app.component';
@@ -9,7 +10,7 @@ import { AddContactComponent }     from './components/add-contact/add-contact.co
 import { EditContactComponent }    from './components/edit-contact/edit-contact.component';
 import { ViewContactComponent }    from './components/view-contact/view-contact.component';
 import { SpinnerComponent }        from './components/spinner/spinner.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PageNotFoundComponent }   from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
