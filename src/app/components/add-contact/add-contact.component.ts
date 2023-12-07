df0d8d07dda48f9223c4e1f45a20c7f93964011a
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-07 14:39:48 -0400

    Component & Function - Add Contact [07/12/2023]

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit }    from '@angular/core';
+import { Router } from '@angular/router';
+import { INaturalPerson }       from 'src/app/models/INaturalPerson';
+import { NaturalPersonService } from 'src/app/services/natural-person.service';
 
 @Component({
-  selector: 'app-add-contact',
-  templateUrl: './add-contact.component.html',
-  styleUrls: ['./add-contact.component.css']
+     selector:  'app-add-contact',
+  templateUrl:  './add-contact.component.html',
+    styleUrls: ['./add-contact.component.css']
 })
-export class AddContactComponent {
+export class AddContactComponent implements OnInit {
+
+  public loading:boolean              = false;
+  public personNatural:INaturalPerson = {} as INaturalPerson;
+  public errorMessage:string | null   = null;
+
+  constructor(private personNaturalService: NaturalPersonService,
+              private router:Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  public create() {
+    this.personNaturalService.create(this.personNatural).subscribe((data) => {
+      this.router.navigate(['/']).then();
+    }, (error) => {
+      this.errorMessage = error;
+      this.router.navigate(['/contacts/add']).then();
+    });
+  }
 
 }
