1388089a7f65d4cfa1dd220a7620069f1e99b342
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-06 20:34:29 -0400

    Interface - Natural Person (Refactor) [06/12/2023]

--- src/app/models/INaturalPerson.ts
+++ src/app/models/INaturalPerson.ts
@@ -1,13 +1,16 @@
 import { Time } from "@angular/common";
 
 export interface INaturalPerson {
-             Rfrnc?:number;
-                 Nm:string;
-            Dscrptn:string;
-    Rfrnc_TypsPrsns:number;
-              Cndtn:number;
-               Rmvd:number;
-               Lckd:number;
-           DtAdmssn:Date;
-             ChckTm:Time;
+         Rfrnc?:number;
+   Rfrnc_Ncnlty:number;
+    Rfrnc_Cntry:number;
+            Nms:string;
+          Srnms:string;
+  RfrntlPhnNmbr:string;
+       TxAddrss:string;
+          Cndtn:number;
+           Rmvd:number;
+           Lckd:number;
+       DtAdmssn:Date;
+         ChckTm:Time;
 }
